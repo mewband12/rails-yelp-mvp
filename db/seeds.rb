ac2278f31ecc47f09bad28c6694e4a65f7a792ce
kandanai leenutaphong <mew.chayutaphong@gmail.com>
2021-08-11 12:20:03 +0700

    d

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+puts "starting..."
+Restaurant.destroy_all
+
+10.times do 
+  Restaurant.create(name: Faker::Book.title, address: Faker::Address.street_address, phone_number: Faker::PhoneNumber.cell_phone_in_e164, category: Restaurant::CATEGORY.sample) 
+end
+puts "Created #{Restaurant.count} restaurants"
